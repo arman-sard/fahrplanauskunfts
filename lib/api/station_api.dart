c69f7eb435d5b46c5e8cf8661c062bb06eaaf9a8
arman <arman@sard.am>
2022-03-14 15:01:37 +0100

    Added try catch statement for error handling

--- lib/api/station_api.dart
+++ lib/api/station_api.dart
@@ -4,21 +4,26 @@ import 'package:http/http.dart' as http;
 
 class StationApi {
   static Future<List<Station>> getStations(String query) async {
-    final url = Uri.parse('https://mvvvip1.defas-fgi.de/mvv/XML_STOPFINDER_REQUEST?language=de&outputFormat=RapidJSON&type_sf=any&name_sf={Suchtext}');
-    final response = await http.get(url);
+    try {
+      final url = Uri.parse('https://mvvvip1.defas-fgi.de/mvv/XML_STOPFINDER_REQUEST?language=de&outputFormat=RapidJSON&type_sf=any&name_sf={Suchtext}');
+      final response = await http.get(url);
 
-    if (response.statusCode == 200) {
-      final data = json.decode(utf8.decode(response.bodyBytes));
-      List<dynamic> stations = data["locations"];
+      if (response.statusCode == 200) {
+        final data = json.decode(utf8.decode(response.bodyBytes));
+        List<dynamic> stations = data["locations"];
 
-      return stations.map((json) => Station.fromJson(json)).where((station) {
-        final nameLower = station.name.toLowerCase();
-        final searchLower = query.toLowerCase();
+        return stations.map((json) => Station.fromJson(json)).where((station) {
+          final nameLower = station.name.toLowerCase();
+          final searchLower = query.toLowerCase();
 
-        return nameLower.contains(searchLower);
-      }).toList();
-    } else {
-      throw Exception();
+          return nameLower.contains(searchLower);
+        }).toList();
+      } else {
+        return [];
+      }
+    } catch (e) {
+      print(e.toString());
+      throw Exception('Could not load the data from api');
     }
   }
 }
